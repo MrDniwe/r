8312fd704480cf8223165e6cbf8619848de7dc3b
Sergei Belevski <ceperagrey@gmail.com>
2019-07-10 14:13:49 +0300

    Решена проблема получения NULL значений из базы

--- internal/article/repository/postgres/postgres.go
+++ internal/article/repository/postgres/postgres.go
@@ -2,9 +2,10 @@ package repository
 
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 	"github.com/mrdniwe/r/internal/models"
 	"github.com/sirupsen/logrus"
+	"html/template"
 )
 
 func NewRepository(client *sql.DB, l *logrus.Logger) (*ArcticleRepo, error) {
@@ -16,16 +17,36 @@ type ArcticleRepo struct {
 	L  *logrus.Logger
 }
 
+type ArticleNullable struct {
+	Id     string
+	Header sql.NullString
+	Lead   sql.NullString
+	Text   sql.NullString
+	Date   pq.NullTime
+	Photo  sql.NullString
+	Views  sql.NullInt64
+}
+
 func (a *ArcticleRepo) GetById(id string) (*models.Article, error) {
 	query := `select 
-	  uuid, title, lead, body, active_from, views
+	  uuid, title, lead, body, active_from, views, image
 	from articles
 	  where is_visible=true
 	  and uuid=$1`
 	row := a.db.QueryRow(query, id)
-	article := &models.Article{}
-	if err := row.Scan(&article.Id, &article.Header, &article.Lead, &article.Text, &article.Date, &article.Views); err != nil {
+	articleNull := ArticleNullable{}
+	if err := row.Scan(&articleNull.Id, &articleNull.Header, &articleNull.Lead, &articleNull.Text, &articleNull.Date, &articleNull.Views, &articleNull.Photo); err != nil {
 		return nil, err
+	}
+	article := &models.Article{
+		Id:      articleNull.Id,
+		Visible: true,
+		Photo:   articleNull.Photo.String,
+		Header:  articleNull.Header.String,
+		Lead:    articleNull.Lead.String,
+		Text:    template.HTML(articleNull.Text.String),
+		Date:    articleNull.Date.Time,
+		Views:   int(articleNull.Views.Int64),
 	}
 	return article, nil
 }
